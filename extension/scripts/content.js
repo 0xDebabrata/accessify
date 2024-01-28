9bdac3817441c31cda71a6462c97960c82352998
Debabrata Mondal <debabrata.js@protonmail.com>
2024-01-28 22:25:53 +0530

    register service worker

---
+++ extension/scripts/content.js
@@ -0,0 +1,59 @@
+const getImages = () => {
+    const imgs = document.getElementsByTagName("img")
+    for (let e of imgs) {
+        console.log(e.alt)
+    }
+}
+
+const handleResponse = (response) => {
+    console.log(response)
+}
+
+const getAnchorTags = () => {
+    const urls = []
+    const links = document.getElementsByTagName("a")
+    const regex = /\bhttps?:\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|]/;
+
+    for (let a of links) {
+        if (regex.test(a.href)) {
+            console.log(a.href)
+            console.log(a.textContent)
+            urls.push(a.href)
+        }
+    }
+
+    chrome.runtime.sendMessage(
+        {
+            urls,
+        },
+        handleResponse
+    )
+
+    return urls
+}
+
+
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
+
+getAnchorTags()
