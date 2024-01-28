8740f5e9d6443d02cdc46ec917b6fbc8e69967f3
Debabrata Mondal <debabrata.js@protonmail.com>
2024-01-28 23:44:23 +0530

    console.log

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -9,10 +9,7 @@ const getImages = () => {
     }
 
     const handleImgResponse = (response) => {
-        for (let i = 0; i < response.length; i++) {
-            anchorTags[i].ariaLabel = response[i]
-        }
-        console.log("Anchor tags updated")
+        console.log(response)
     }
 
     chrome.runtime.sendMessage(
@@ -51,30 +48,6 @@ const getAnchorTags = () => {
     )
 
     return urls
-}
-
-
-const article = document.querySelector("article");
-
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
 }
 
 getAnchorTags()
