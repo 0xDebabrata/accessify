336493b6047670a829ea107e64095a2edddb58e9
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-01-30 22:25:06 +0530

    REMOVED: express js server

--- server/src/environment.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const PORT = process.env.PORT || 3000
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY
-export const GPT_CHAT_URL = 'https://api.openai.com/v1/chat/completions'
